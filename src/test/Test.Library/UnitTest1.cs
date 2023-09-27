3de223b048c7136e864dd7b6658bd5e80f45f072
Stefano <stefano.francolino@correo.ucu.edu.uy>
2023-09-27 10:40:39 -0300

    k

--- src/test/Test.Library/UnitTest1.cs
+++ src/test/Test.Library/UnitTest1.cs
@@ -2,17 +2,38 @@ using NUnit.Framework;
 
 namespace Test.Library
 {
-    public class Tests
+    public class RoleplayGameTests
     {
-        [SetUp]
-        public void Setup()
-        {
+        /*
+            JUSTIFICACIÓN:
+            Este caso de prueba lo que revisa es si, al agregar un aspecto tan importante
+            como el nombre de un personaje, es vacio o nulo.
+            Si el llamado de la función devuelve TRUE, significa que el nombre pasó las validaciones correspondientes.
+            Si devuelve FALSE, es sinonimo que el nombre no es valido para el personaje. 
+        */
+        [Test]
+        public void TestBlankWizardName() {
+            Wizard wiz = new Wizard();
+            string newWizardName = "";
+
+            bool result = wiz.SetName(newWizardName);
+            Assert.False(result);
         }
 
+        /*
+            JUSTIFICACIÓN:
+            Este test se encarga de realizar una prueba, de modo que no pueden haber 2 o 
+            más hechizos con el mismo nombre, ya que si el nombre se repite 2 veces,
+            el result2(último hechizo agregado) se considerará falso
+        */
         [Test]
-        public void Test1()
-        {
-            Assert.Pass();
+        public void TestDuplicatedSpell(){
+            SpellBook book = new SpellBook();
+            Spell sp = new Spell("Heal");
+
+            bool result = book.AddSpell(sp);
+            bool result2 = book.AddSpell(sp);
+            Assert.False(result2);
         }
     }
 }
