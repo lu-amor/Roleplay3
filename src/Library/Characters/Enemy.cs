1221c0fbf2bf87ee94ea91426367f3820f09e4e2
felieppe <me@felieppe.com>
2023-10-02 19:44:21 -0300

    Enemy class added

---
+++ src/Library/Characters/Enemy.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoleplayGame
+{
+    public class Enemy : IEnemy
+    {
+        
+
+        public void SetVictoryPoints(int points) {
+
+        }
+    }
+}
