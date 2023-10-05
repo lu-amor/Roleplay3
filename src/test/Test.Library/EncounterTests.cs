abcab89e1ecac9a13595abbd9a7edaea9fa1583d
Felipe <me@felieppe.com>
2023-10-04 22:11:24 -0300

    Removed test case

--- src/test/Test.Library/EncounterTests.cs
+++ src/test/Test.Library/EncounterTests.cs
@@ -2,17 +2,5 @@ using Microsoft.Win32.SafeHandles;
 
 namespace RoleplayGame
 {
-    public class EncounterTests : Encounter
-    {   
-        [Test]
-        public void DwarfReceiveAttack() {
-            Dwarf Dw = new Dwarf("Pepe");
-            Dw.SetVictoryPoints(5);
-
-            Skeleton Sk = new Skeleton("Juan");
-            Sk.SetVictoryPoints(2);
-        
-            Heroes.AddHeroe(Dwarf Dw);
-        }
-    }
+    public class EncounterTests : Encounter {}
 }
