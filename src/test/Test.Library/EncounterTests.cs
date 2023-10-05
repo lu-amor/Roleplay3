65007d17bb85311230e0aab6650943e05b4f3cd5
Felipe <me@felieppe.com>
2023-10-04 22:28:58 -0300

    MissingHeroInEncounter() test case added

--- src/test/Test.Library/EncounterTests.cs
+++ src/test/Test.Library/EncounterTests.cs
@@ -1,4 +1,19 @@
+using System.Reflection;
+
 namespace RoleplayGame
 {
-    public class EncounterTests : Encounter {}
+    public class EncounterTests : Encounter {
+        [Test]
+        public void MissingHeroInEncounter() {
+            Enemy enemy = new Enemy("Alfred");
+            Heroe hero = new Heroe("Andres");
+
+            Encounter enc = new Encounter();
+            enc.AddEnemy(enemy);
+            // enc.AddHeroe(hero);
+
+            bool statusEncounter = enc.CheckFighters();
+            Assert.False(statusEncounter);
+        }
+    }
 }
