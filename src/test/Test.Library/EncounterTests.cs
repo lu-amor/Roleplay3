c19461eedf4159db91460a60eae4b18f759d9058
Felipe <me@felieppe.com>
2023-10-04 22:33:38 -0300

    Revert "MissingHeroInEncounter() test case added"
    This reverts commit 65007d17bb85311230e0aab6650943e05b4f3cd5.

--- src/test/Test.Library/EncounterTests.cs
+++ src/test/Test.Library/EncounterTests.cs
@@ -1,19 +1,4 @@
-using System.Reflection;
-
 namespace RoleplayGame
 {
-    public class EncounterTests : Encounter {
-        [Test]
-        public void MissingHeroInEncounter() {
-            Enemy enemy = new Enemy("Alfred");
-            Heroe hero = new Heroe("Andres");
-
-            Encounter enc = new Encounter();
-            enc.AddEnemy(enemy);
-            // enc.AddHeroe(hero);
-
-            bool statusEncounter = enc.CheckFighters();
-            Assert.False(statusEncounter);
-        }
-    }
+    public class EncounterTests : Encounter {}
 }
