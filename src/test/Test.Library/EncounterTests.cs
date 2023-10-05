36ebb1c79144002c54a65cf34cf401921697c6cd
Felipe <me@felieppe.com>
2023-10-04 22:59:54 -0300

    MissingHeroInEncounter() test case finished

--- src/test/Test.Library/EncounterTests.cs
+++ src/test/Test.Library/EncounterTests.cs
@@ -2,5 +2,18 @@ using NUnit.Framework;
 
 namespace RoleplayGame
 {
-    public class EncounterTests {}
+    public class EncounterTests {
+        [Test]
+        public void MissingHeroInEncounter() {
+            Orc enemy = new Orc("Alfred");
+            Archer hero = new Archer("Andres");
+
+            Encounter enc = new Encounter();
+            enc.AddEnemy(enemy);
+            // enc.AddHeroe(hero);
+
+            bool statusEncounter = false; //enc.CheckFighters();
+            Assert.False(statusEncounter);
+        }
+    }
 }
